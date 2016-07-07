7288702c5560388b545d932b6c19eadb51ebb91d
ccruz07 <cruz1706@gmail.com>
2016-07-06 19:34:18 -0700

    Questions have own functions

--- game.js
+++ game.js
@@ -1,1 +1,36 @@
-game.js
+var answerCount = 0;
+var userName = prompt ("Hey who you!");
+alert ("Hey " + userName + "! How you livin !");
+
+teaching();
+school();
+number();
+var finalCount = alert("Good job you guessed " + answerCount + " correct");
+
+function teaching(){
+  var years = prompt ("How long have you been teaching?");
+  if (years > 10){
+    alert("CORRECT!");
+    answerCount++;
+  }else{
+    alert("Sorry Charlie!");
+  }
+}
+
+function school(){
+  var schoolName = prompt ("What school did you attend?");
+  if (schoolName == "Nova" || school == "SUNY"){
+    alert ("CORRECT");
+    answerCount++;
+  }else{
+    alert ("Sorry Charlie!");
+  }
+}
+
+function number(){
+  var number = prompt ("What is my favorite number?");
+  if(number == "July 24"){
+    alert ("CORRECT!");
+    answerCount++;
+  }
+}
