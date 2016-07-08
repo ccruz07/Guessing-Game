c45b5cbc473bcc31c7a0749d55e0ed6a3c2a4787
ccruz07 <cruz1706@gmail.com>
2016-07-07 19:37:03 -0700

    Updates for Class-4, functions and CSS

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 var answerCount = 0;
-var userName = prompt ("Hey who you!");
+//var userName = prompt ("Hey who you!");
 alert ("Hey " + userName + "! How you livin !");
 
 function playGame(){
-  askQuestion("How long have you been teaching?", 10);
-  askQuestion("What school did you attend?", "Nova" );
+  askQuestion("Hey who you!", "userName");
+  askQuestion("How long have I been teaching?", 10);
+  askQuestion("What school did I attend?", "Nova" );
   askQuestion("What is my favorite date?", "July 24");
+  askQuestion("Name my hobbies", "Sewing,Jewelry,Shopping");
   var finalCount = alert("Good job you guessed " + answerCount + " correct");
 }
 
